ea24a6b96f326ce33a818efa1fc2535d97178a04
Simon Janlöv <simon.janlov@gmail.com>
2023-10-17 13:41:58 +0200

    reworked the create_image function to fit the image and check for errors

--- print_image_as_array.py
+++ print_image_as_array.py
@@ -1,13 +1,12 @@
 import numpy as np
 from PIL import Image
 
-def print_image_array(image_path):
+def create_image_array(image_path):
     """Takes an image path as an argument, saves the image as an array and prints it"""
     image = Image.open(image_path)
     data = np.asarray(image)
-    print(data)
     return data
 
 
 if __name__ == "__main__":
-    print_image_array('test_image.jpg')
+    create_image_array('test_image.jpg')
