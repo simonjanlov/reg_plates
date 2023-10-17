bc866f1043b4acce9f7719cf199a52412725bc2f
Henry Apler <henry.apler@gmail.com>
2023-10-17 14:18:27 +0200

    made it to a function

--- generate_reg_no.py
+++ generate_reg_no.py
@@ -1,41 +1,44 @@
 import random
 
-letters ='ABCDEFGHJKLMNOPRSTUWXYZ'
-chars = "0123456789ABCDEFGHJKLMNPRSTUWXZ"
-numbers = "0123456789"
+def generate_reg_nr():
+    letters ='ABCDEFGHJKLMNOPRSTUWXYZ'
+    chars = "0123456789ABCDEFGHJKLMNPRSTUWXZ"
+    numbers = "0123456789"
 
 
-error_list = ['APA','ARG','ASS','BAJ','BSS','CUC','CUK','DUM','ETA',
-            'ETT','FAG','FAN','FEG','FEL','FEM','FES','FET','FNL',
-            'FUC','FUK','FUL','GAM','GAY','GEJ','GEY','GHB','GUD',
-            'GYN','HAT','HBT','HKH','HOR','HOT','KGB','KKK','KUC',
-            'KUF','KUG','KUK','KYK','LAM','LAT','LEM','LOJ','LSD',
-            'LUS','MAD','MAO','MEN','MES','MLB','MUS','NAZ','NRP',
-            'NSF','NYP','OND','OOO','ORM','PAJ','PKK','PLO','PMS',
-            'PUB','RAP','RAS','ROM','RPS','RUS','SEG','SEX','SJU',
-            'SOS','SPY','SUG','SUP','SUR','TBC','TOA','TOK','TRE',
-            'TYP','UFO','USA','WAM','WAR','WWW','XTC','XTZ','XXL',
-            'XXX','ZEX','ZOG','ZPY','ZUG','ZUP'	'ZOO']
-part2_reg_nr = ""
-part1_reg_nr = ""
-flag = True
-while flag:
-    for i in range(3):
-        part1_reg_nr += random.choice(letters)
+    error_list = ['APA','ARG','ASS','BAJ','BSS','CUC','CUK','DUM','ETA',
+                'ETT','FAG','FAN','FEG','FEL','FEM','FES','FET','FNL',
+                'FUC','FUK','FUL','GAM','GAY','GEJ','GEY','GHB','GUD',
+                'GYN','HAT','HBT','HKH','HOR','HOT','KGB','KKK','KUC',
+                'KUF','KUG','KUK','KYK','LAM','LAT','LEM','LOJ','LSD',
+                'LUS','MAD','MAO','MEN','MES','MLB','MUS','NAZ','NRP',
+                'NSF','NYP','OND','OOO','ORM','PAJ','PKK','PLO','PMS',
+                'PUB','RAP','RAS','ROM','RPS','RUS','SEG','SEX','SJU',
+                'SOS','SPY','SUG','SUP','SUR','TBC','TOA','TOK','TRE',
+                'TYP','UFO','USA','WAM','WAR','WWW','XTC','XTZ','XXL',
+                'XXX','ZEX','ZOG','ZPY','ZUG','ZUP'	'ZOO']
+    part2_reg_nr = ""
+    part1_reg_nr = ""
+    flag = True
+    while flag:
+        for i in range(3):
+            part1_reg_nr += random.choice(letters)
 
-    for i in range(2):
-        part2_reg_nr +=  random.choice(numbers)
+        for i in range(2):
+            part2_reg_nr +=  random.choice(numbers)
 
-    for i in range(1):
-        part2_reg_nr += random.choice(chars)
+        for i in range(1):
+            part2_reg_nr += random.choice(chars)
 
-    
-    if part1_reg_nr not in error_list:
-        flag = False
-
-
-reg_nr = part1_reg_nr+" "+part2_reg_nr
-print(reg_nr)
+        
+        if part1_reg_nr not in error_list:
+            flag = False
 
 
+    reg_nr = part1_reg_nr+" "+part2_reg_nr
+    return reg_nr
+
+if __name__ == "__main__":
+    reg_nr = generate_reg_nr()
+    print(reg_nr)
 
