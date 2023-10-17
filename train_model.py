10dccbe4a6329050f4ed7316f6347405c9c2bdb5
Simon Janlöv <simon.janlov@gmail.com>
2023-10-17 19:41:41 +0200

    train_model.py - create empty DFs first before we append

--- train_model.py
+++ train_model.py
@@ -2,9 +2,11 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from convert_to_array import create_image_array
 
+df_data = pd.DataFrame()
+df_label = pd.DataFrame()
 
-df_data = pd.DataFrame(create_image_array('test_plate.png'))
-df_label = pd.DataFrame(['A'])
+df_data = pd.concat([df_data, pd.DataFrame(create_image_array('test_plate.png'))], axis=0)
+df_label = pd.concat([df_label, pd.DataFrame(['A'])], axis=0)
 
 df_data = pd.concat([df_data, pd.DataFrame(create_image_array('test_plate2.png'))], axis=0)
 df_label = pd.concat([df_label, pd.DataFrame(['B'])], axis=0)
