2ebd8903e6546724cb670d86cdbed206769251d1
Henry Apler <henry.apler@gmail.com>
2023-10-17 15:41:59 +0200

    added line in array func

--- print_image_as_array.py
+++ print_image_as_array.py
@@ -4,9 +4,10 @@ from PIL import Image
 def create_image_array(image_path):
     """Takes an image path as an argument, saves the image as an array and prints it"""
     image = Image.open(image_path)
-    data = np.asarray(image)
+    data = np.asarray(image).reshape(1,-1)
+    
     return data
 
 
 if __name__ == "__main__":
-    create_image_array('test_image.jpg')
+    print(create_image_array('test_plate.png'))
