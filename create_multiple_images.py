a35730a29e6a3ec666f18263bf6fa8510ef12210
Henry Apler <henry.apler@gmail.com>
2023-10-18 08:50:51 +0200

    added a progressbar to create_multiple_images,lets see if it works

--- create_multiple_images.py
+++ create_multiple_images.py
@@ -2,17 +2,20 @@ from create_image import create_image
 from create_folder import create_folders 
 from generate_reg_no import generate_reg_nr
 from config import *
+from rich.progress import track # progressbar
+
 
 
 def create_multiple_images(amount):
     create_folders(FOLDER_PATH)
-    
-    for i in range(amount):
-        file_name = str(i + 1) + '.png'
-        current_plate_number = generate_reg_nr()
-        first_letter = current_plate_number[0]
-        full_path = FOLDER_PATH + first_letter + '\\' + file_name
-        create_image(current_plate_number, full_path)
+    for n in track(range(amount), description='creating image...'):
+        # Here, the create-image code is going
+        for i in range(amount):
+            file_name = str(i + 1) + '.png'
+            current_plate_number = generate_reg_nr()
+            first_letter = current_plate_number[0]
+            full_path = FOLDER_PATH + first_letter + '\\' + file_name
+            create_image(current_plate_number, full_path)
 
 
 
